ec4cc8edaf023c4db675896fadfdaddbc00beb25
Sebastien G <sebastien.gaudard.1@gmail.com>
2020-07-10 21:28:59 +0200

    Add main data to app

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,55 @@
     </div>
 </template>
 
+<script>
+    import uuid from 'uuid'
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                alcoholRate: 60,
+                softs:
+                    [{
+                        id: 0,
+                        name: 'Coca'
+                    }, {
+                        id: 1,
+                        name: 'Orangina'
+                    }, {
+                        id: 2,
+                        name: 'Jus d\'orange'
+                    }],
+                alcohol:
+                    [{
+                        id: 0,
+                        name: 'Vodka'
+                    }, {
+                        id: 1,
+                        name: 'Whisky'
+                    }, {
+                        id: 2,
+                        name: 'Get27'
+                    }, {
+                        id: 3,
+                        name: 'Rhum'
+                    }]
+            }
+        },
+        methods: {
+            addSoft: function (name) {
+                this.softs = [{id: uuid.v4(), name}, ...this.softs]
+            },
+            addAlcohol: function (name) {
+                this.softs = [{id: uuid.v4(), name}, ...this.alcohol]
+            }
+        },
+        mounted() {
+            console.log(this.softs)
+        }
+    }
+</script>
+
 <style lang="scss">
     @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap');
     @import './assets/css/style.css';
